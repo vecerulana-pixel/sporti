bb2d52cda311accd94290e2765a780828f020e5a
Tmrgg96 <tmrgg@yandex.ru>
2026-07-23 21:56:51 +0500

    Add launcher icon and bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.tacktikcwin.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildFeatures {
